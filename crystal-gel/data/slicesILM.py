60ac96c112bfbfb8aa6d9e732c0d0b26132270f3
mathieu <mathieu@d0924afd-704d-4a38-b079-49faeb141ace>
2016-06-24 09:40:28 +0000

    thinner slices (100<z<125) data from highcp sample to generate the slice through povray

--- crystal-gel/data/slicesILM.py
+++ crystal-gel/data/slicesILM.py
@@ -8,23 +8,24 @@ import subprocess
 #1 -> HCP
 #2 -> FCC
 
-for t in [120, 390, 890]:
-    pos = np.loadtxt('pos_%03d'%t, skiprows=1)
-    sl = (pos[:,-1]>100) & (pos[:,-1]<150)
-    pos = pos[sl]
-    phase = np.loadtxt('studyphase_%03d'%t, dtype=int, usecols=[1])[sl]
-    
-    f = File("sliceILM_t%03d.pov"%t,"colors.inc", "header.inc")
-    crystals = Union(*[
-        Sphere( (x,y,z), 5)
-        for x,y,z in pos[phase<3]
-        ]+[Texture(Pigment(color=(0.694,  0.145,  0.623)))])
-    f.write(crystals)
-    for i, c in zip([3,4,5], [(0.847,0.57,0.81), (0.847,0.57,0.81), (1.,  0.455,  0.156)]):
-        f.write(Union(*[
+for sample, times in zip(['', '_highcp'], [[120, 390, 890], [800]]):
+    for t in times:
+        pos = np.loadtxt('pos%s_%03d'%(sample, t), skiprows=1)
+        sl = (pos[:,-1]>100) & (pos[:,-1]<125)
+        pos = pos[sl]
+        phase = np.loadtxt('studyphase%s_%03d'%(sample, t), dtype=int, usecols=[1])[sl]
+        
+        f = File("sliceILM%s_t%03d.pov"%(sample, t),"colors.inc", "header.inc")
+        crystals = Union(*[
             Sphere( (x,y,z), 5)
-            for x,y,z in pos[phase==i]
-            ]+[Texture(Pigment(color=c))
-            ]))
-    f.file.flush()
-    subprocess.call(['povray', '-O../presentation/sliceILM_t%03d.png'%t, '+W256', '+H256' , 'sliceILM_t%03d.pov'%t])
+            for x,y,z in pos[phase<3]
+            ]+[Texture(Pigment(color=(0.694,  0.145,  0.623)))])
+        f.write(crystals)
+        for i, c in zip([3,4,5], [(0.847,0.57,0.81), (0.847,0.57,0.81), (1.,  0.455,  0.156)]):
+            f.write(Union(*[
+                Sphere( (x,y,z), 5)
+                for x,y,z in pos[phase==i]
+                ]+[Texture(Pigment(color=c))
+                ]))
+        f.file.flush()
+        subprocess.call(['povray', '-O../presentation/sliceILM%s_t%03d.png'%(sample, t), '+W256', '+H256' , 'sliceILM%s_t%03d.pov'%(sample, t)])
